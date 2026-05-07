5ae3d682a8bc24913ef02195549bfe2723eaea35
nascimennttoo <rafaelnascimento1502@gmail.com>
2026-05-06 22:33:26 -0300

    Conditional Structure

--- JavaStudies/src/conditional_structure/ex5.java
+++ JavaStudies/src/conditional_structure/ex5.java
@@ -11,14 +11,14 @@ public class ex5 {
         double total;
 
 
-        System.out.println("===============================");
-        System.out.println("|CODE | SPECIFICATION | PRICE |");
-        System.out.println("| 01  |  Hot Dog      |U$4.00 |");
-        System.out.println("| 02  |  X-Salad      |U$4.50 |");
-        System.out.println("| 03  |  X-Bacon      |U$5.00 |");
-        System.out.println("| 04  |  Simple toast |U$2.00 |");
-        System.out.println("| 05  |  Soda         |U$1.50 |");
-        System.out.println("===============================");
+        System.out.println("=================================");
+        System.out.println("||CODE | SPECIFICATION | PRICE ||");
+        System.out.println("|| 01  |  Hot Dog      |U$4.00 ||");
+        System.out.println("|| 02  |  X-Salad      |U$4.50 ||");
+        System.out.println("|| 03  |  X-Bacon      |U$5.00 ||");
+        System.out.println("|| 04  |  Simple toast |U$2.00 ||");
+        System.out.println("|| 05  |  Soda         |U$1.50 ||");
+        System.out.println("=================================");
 
         System.out.println("Enter the code and quantity: ");
         code = sc.nextInt();
